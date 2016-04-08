3f73f81f463d9fbfdd8b9fac38dfba170703dddd
kenve <zwei.xie@gmail.com>
2016-04-08 17:32:34 +0800

    update App.jsx

--- demo01/app/components/App.jsx
+++ demo01/app/components/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React , { Component } from 'react';
 import Note from './Note.jsx';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return <Note />;
   }
